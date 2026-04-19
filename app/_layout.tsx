4b15e57290814d95bf09f0d93b3c047e774089b1
MehmetHooke <hoke628@gmail.com>
2026-04-19 14:53:50 +0300

    (auth):added login register page

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,25 +1,20 @@
+import { useAuth } from "@/src/context/AuthContext";
 import "@/src/i18n";
-import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
-import { Stack } from 'expo-router';
-import { StatusBar } from 'expo-status-bar';
-import 'react-native-reanimated';
+import { Stack } from "expo-router";
 
-import { useColorScheme } from '@/hooks/use-color-scheme';
-
-export const unstable_settings = {
-  anchor: '(tabs)',
-};
 
 export default function RootLayout() {
-  const colorScheme = useColorScheme();
 
+  const { user, loading } = useAuth();
+  if (loading) return null;
   return (
-    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-      <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen name="modal" options={{ presentation: 'modal', title: 'Modal' }} />
-      </Stack>
-      <StatusBar style="auto" />
-    </ThemeProvider>
+    <Stack>
+      {!user ? (
+        <Stack.Screen name="(auth)"/>
+      ):(
+        <Stack.Screen name="(tabs)"/>
+
+      )}
+    </Stack>
   );
 }
