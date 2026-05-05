4ba112a26fc6470d184a801c94d36f614d241bff
MehmetHooke <hoke628@gmail.com>
2026-05-05 16:03:00 +0300

    onboarding show always disabled, onboarding compleated now

--- src/services/onboarding.ts
+++ src/services/onboarding.ts
@@ -3,9 +3,8 @@ import AsyncStorage from "@react-native-async-storage/async-storage";
 const ONBOARDING_COMPLETED_KEY = "recallly-onboarding-completed";
 
 export async function getOnboardingCompleted() {
-  //   const value = await AsyncStorage.getItem(ONBOARDING_COMPLETED_KEY);
-  //   return value === "true";
-  return false;
+  const value = await AsyncStorage.getItem(ONBOARDING_COMPLETED_KEY);
+  return value === "true";
 }
 
 export async function setOnboardingCompleted() {
